3ac5a3e556c515a250cb99a5ce545590059af5eb
Sam-Freitas <samfreitas@email.arizona.edu>
2021-11-24 16:31:07 -0700

    Update open_webcam_test.py

--- open_webcam_test.py
+++ open_webcam_test.py
@@ -6,17 +6,12 @@ cam = VideoCapture(0)  #set the port of the camera as before
 
 sz = 512
 
-for i in range(100):
-    retval, image = cam.read() #return a True bolean and and the image if all go right
-    if i == 0:
-        width, height, depth = image.shape   # Get dimensions
-        left = int((width - sz)/2)
-        top = int((height - sz)/2)
-        right = int((width + sz)/2)
-        bottom = int((height + sz)/2)
+num_frames = 1000
 
-    im = image[left:right,top:bottom,0:depth]
-    im = cv2.flip(im, 1)
+for i in range(num_frames):
+    retval, image = cam.read() #return a True bolean and and the image if all go right
+
+    im = cv2.flip(image, 1)
     
     imshow(str(retval),im)
     cv2.setWindowProperty(str(retval), cv2.WND_PROP_TOPMOST, 1)
